a65cf2aa66c4207468c77f1d333c2f426caf849d
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-04 13:13:07 -0600

    !!

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function guessNumber(guess) {
-  if (price == 25) {
+  if (price != 25) {
     return "not it";
   }
   }
 function strictGuessNumber(guess) {
-  if (guess == 25) {
+  if (guess !== 25) {
     return "not it";
   }
 }
+
 
 
